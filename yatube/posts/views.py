a83d89f6d6bf2c9366ab253b7f0b73045ec4bb0a
Любовь Беркова <grandprinceess7777@gmail.com>
2022-02-09 13:37:32 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
 # Главная страница
-def index(request):    
+def index(request):
     return HttpResponse('Главная страница')
 
 # Страница сообщества
-def group_posts(request, pk):
-    return HttpResponse(f'Страница сообщества {pk}')
+def group_posts(request, slug):
+    return HttpResponse(f'Страница сообщества {slug}')
+
