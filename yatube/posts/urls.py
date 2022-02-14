d7b1c3eb83c4e3e9d5881664cc282b1eddf70761
Любовь Беркова <grandprinceess7777@gmail.com>
2022-02-14 15:46:49 +0300

    Первые наброски

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'posts'),
-    path('group/<slug:slug>/', views.group_posts, name= 'group_posts'),
+    path('', views.index),
+    path('community_page', views.group_list),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,21 +2,14 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 
+
 # Главная страница
-def index(request):
-    return HttpResponse('Главная страница')
-
-# Страница сообщества
-def group_posts(request, slug):
-    return HttpResponse(f'Страница сообщества {slug}')
-
-def index(request):
+def index(request):   
     template = loader.get_template('posts/index.html')
     return HttpResponse(template.render({}, request))
 
-def group_posts(request, slug):
-    return HttpResponse(f'Посты по группам {slug}')
-    
-def group_posts(request):
+
+# Страница сообщества
+def group_list(request):
     template = 'posts/group_list.html'
     return render(request, template)
