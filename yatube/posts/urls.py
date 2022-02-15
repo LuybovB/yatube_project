6f62842ead8d9612b1bc43e127591ce47c9387fa
Любовь Беркова <grandprinceess7777@gmail.com>
2022-02-15 14:38:55 +0300

    Добавлено: Ссылки, namespaсe и name.

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('posts', views.group_list, name='group_name'),
+    path('', views.index, name=''),
+    path('posts/', views.group_list, name='group_name'),
+    path('group/<slug:slug>/', views.group_posts),
 ]
