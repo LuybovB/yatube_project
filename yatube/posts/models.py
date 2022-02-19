9701c4d2fd498afc623151969ed8b81d1327fd1a
Любовь Беркова <grandprinceess7777@gmail.com>
2022-02-19 22:47:00 +0300

    комментарий

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,23 +5,23 @@ User = get_user_model()
 
 
 class Group(models.Model):
-
     title = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
     description = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
-    
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name = 'posts')
+        related_name='posts')
     group = models.ForeignKey(
         Group,
         on_delete=models.CASCADE,
-        related_name = 'posts',
+        related_name='posts',
         blank=True, null=True, max_length=100)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
+
 
 from .models import Post
 from .models import Group
-# Главная страница
+
+
 def index(request):
     template = 'posts/index.html'
-    posts = Post.objects.order_by('-pub_date')[:3]
+    posts = Post.objects.order_by('-pub_date')[:10]
     title = 'Главная страница'
     text = 'Последние обновления на сайте'
     context = {
@@ -16,27 +17,28 @@ def index(request):
     }
     return render(request, template, context)
 
+
 def group_posts(request, slug):
     template = 'posts/group_posts.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:3]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
         'group': group,
         'posts': posts,
     }
     return render(request, template, context)
 
+
 def group_list(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:3]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     title = 'Страница сообщества'
     text = 'Лев Толстой – зеркало русской революции.'
-
     context = {
         'group': group,
         'posts': posts,
         'title': title,
         'text': text,
-    }    
+    }
     return render(request, template, context)
